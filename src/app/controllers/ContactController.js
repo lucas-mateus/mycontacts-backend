584534088ae655119bf6a1a006b818be6bc63a0e
lucas-mateus <xlucasmat@gmail.com>
2023-03-20 12:38:50 -0300

    feat: create the Controller layer of application

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,96 @@
+const ContacstRepository = require('../repositories/ContactsRepository');
+
+class ContactController {
+  async index(request, response) {
+    // Listar todos os registros
+    const contacts = await ContacstRepository.findAll();
+    response.json(contacts);
+  }
+
+  async show(request, response) {
+    // Obter UM REGISTRO
+    const { id } = request.params;
+    const contact = await ContacstRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: 'Contact not found' });
+    }
+
+    return response.json(contact);
+  }
+
+  async store(request, response) {
+    // Criar novo registro
+    const { name, email, phone, category_id } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required.' });
+    }
+
+    const contactExists = await ContacstRepository.findByEmail(email);
+
+    if (contactExists) {
+      return response
+        .status(400)
+        .json({ error: 'This e-mail is already been taken.' });
+    }
+
+    const contact = await ContacstRepository.create({
+      name,
+      email,
+      phone,
+      category_id,
+    });
+
+    return response.status(200).json(contact);
+  }
+
+  async update(request, response) {
+    // Atualizar um registro
+
+    const { id } = request.params;
+    const { name, email, phone, category_id } = request.body;
+
+    const contactExists = await ContacstRepository.findById(id);
+
+    if (!contactExists) {
+      return response.status(404).json({ error: 'Contact not found.' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required.' });
+    }
+    const contactByEmail = await ContacstRepository.findByEmail(email);
+
+    if (contactByEmail && contactByEmail.id !== id) {
+      return response
+        .status(400)
+        .json({ error: 'This e-mail is already been taken.' });
+    }
+
+    const contact = await ContacstRepository.update(id, {
+      name,
+      email,
+      phone,
+      category_id,
+    });
+
+    return response.status(200).json(contact);
+  }
+
+  async delete(request, response) {
+    // Deletar um registro
+    const { id } = request.params;
+    const contact = await ContacstRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    await ContacstRepository.delete(id);
+
+    return response.sendStatus(204);
+  }
+}
+
+module.exports = new ContactController();
