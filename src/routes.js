905861ac5924541cf37409ca37f789c22b4513f3
lucas-mateus <xlucasmat@gmail.com>
2023-03-20 19:06:24 -0300

    fix: create the routes to handle with categories

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
 const { Router } = require('express');
 const ContactController = require('./app/controllers/ContactController');
+const CategoryController = require('./app/controllers/CategoryController');
 
 const router = Router();
+
+// Contacts routes
 
 router.get(
   '/contacts',
@@ -14,5 +17,13 @@ router.post('/contacts', ContactController.store);
 router.get('/contacts/:id', ContactController.show);
 router.put('/contacts/:id', ContactController.update);
 router.delete('/contacts/:id', ContactController.delete);
+
+// Categories routes
+
+router.get('/categories', CategoryController.index);
+router.get('/categories/:id', CategoryController.show);
+router.post('/categories', CategoryController.store);
+router.put('/categories/:id', CategoryController.update);
+router.delete('/categories/:id', CategoryController.delete);
 
 module.exports = router;
